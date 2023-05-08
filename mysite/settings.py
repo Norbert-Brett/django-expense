9ebc5f1641a3e10c8a442d369eefb82733f36353
Norbert-Brett <roco_norbi@yahoo.com>
2023-05-08 22:01:44 +0100

    deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,9 +19,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-expense-production.up.railway.app']
 
-# CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://django-expense-production.up.railway.app']
 
 
 # Application definition
